be1e154b5795115d3a21f034b46277ce68e03663
ek-ysakuma <89017340+ek-ysakuma@users.noreply.github.com>
2026-07-17 20:21:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/kyoto-connect-grow/', 
